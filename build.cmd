e6c94abed12ccc193e9bc38f32fb7d39c74a8434
mavnn <michael@mavnn.co.uk>
2013-12-17 12:25:35 +0000

    Remove pause command from build.cmd

--- build.cmd
+++ build.cmd
@@ -3,4 +3,3 @@ if not exist packages\FAKE\tools\Fake.exe (
   .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion -Prerelease
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
-pause
