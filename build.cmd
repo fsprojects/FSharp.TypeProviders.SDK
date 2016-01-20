95883fb6d65feefa1cd05ae66c924148c69859dc
mavnn <michael@mavnn.co.uk>
2016-01-20 10:04:14 +0000

    Fix NUnit.Runners versions until v3 supported

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ if not exist packages\FAKE\tools\Fake.exe (
   .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 )
 if not exist packages\NUnit.Runners\tools\nunit.exe ( 
-  .nuget\nuget.exe install Nunit.Runners -OutputDirectory packages -ExcludeVersion
+  .nuget\nuget.exe install Nunit.Runners -OutputDirectory packages -ExcludeVersion -Version 2.6.4
 )
 
 packages\FAKE\tools\FAKE.exe build.fsx %*
