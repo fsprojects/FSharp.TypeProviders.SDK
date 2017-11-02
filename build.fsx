302ea984ec1afb03d3b5f67f79e9de1aa21c3164
dsyme <dsyme@MSRC-4079076>
2017-11-02 16:45:29 +0000

    build examples

--- build.fsx
+++ build.fsx
@@ -71,18 +71,17 @@ Target "Restore" (fun _ ->
     exec "dotnet" "restore"
 )
 Target "Build" (fun _ ->
-    exec "dotnet" ("build -c "+config)
+    exec "dotnet" ("build -c "+config+" src/FSharp.TypeProviders.SDK.fsproj")
+    exec "dotnet" ("build -c "+config+" tests/FSharp.TypeProviders.SDK.Tests.fsproj")
 )
 
-//#if EXAMPLES
-//            { Name = "StaticProperty"; ProviderSourceFiles = ["StaticProperty.fsx"]; TestSourceFiles = ["StaticProperty.Tests.fsx"]}
-//            { Name = "ErasedWithConstructor"; ProviderSourceFiles = ["ErasedWithConstructor.fsx"]; TestSourceFiles = ["ErasedWithConstructor.Tests.fsx"]}
-//#endif
-
+Target "Examples" (fun _ ->
+    exec "dotnet" ("build -c "+config+" examples/BasicProvider.Tests/BasicProvider.Tests.fsproj")
+)
 Target "RunTests" (fun _ ->
 #if MONO
   // We don't use dotnet test because of https://github.com/dotnet/sdk/issues/335
-  // FAKE probably helps us find this test file or arch?
+  // This is a bit of a hack to find the output tesat DLL and run xunit using Mono directly
     let dir = "tests/bin/" + config + "/net461"
     let file = 
         System.IO.Directory.GetDirectories(dir)
@@ -114,10 +113,8 @@ Target "NuGet" (fun _ ->
 
 "Restore"
     ==> "Build"
-//#if EXAMPLES
-//    ==> "Examples"
-//#endif
     ==> "RunTests"
+    ==> "Examples"
     ==> "NuGet"
 
 RunTargetOrDefault "RunTests"
