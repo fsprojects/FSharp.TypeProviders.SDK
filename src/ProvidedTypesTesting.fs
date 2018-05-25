4b9ce4b75b6fd727a96378eb4c0f303638db2e71
Don Syme <donsyme@fastmail.fm>
2018-05-25 17:43:19 +0200

    update to testing file

--- src/ProvidedTypesTesting.fs
+++ src/ProvidedTypesTesting.fs
@@ -104,6 +104,19 @@ type internal Testing() =
         let maxWidth = defaultArg maxWidth 100
         let useQualifiedNames = defaultArg useQualifiedNames false
 
+        let hasUnitOfMeasure (t: System.Type) = 
+            t.IsGenericType && 
+            (t.Namespace = "System") &&
+            (t.Name = typeof<bool>.Name ||
+             t.Name = typeof<obj>.Name ||
+             t.Name = typeof<int>.Name ||
+             t.Name = typeof<int64>.Name ||
+             t.Name = typeof<float>.Name ||
+             t.Name = typeof<float32>.Name ||
+             t.Name = typeof<decimal>.Name)
+
+        let isFSharpUnitAnnotated (t:System.Type) = t.IsGenericType && not (t.GetGenericTypeDefinition().IsGenericTypeDefinition)
+
         let knownNamespaces =
             [ t.Namespace
               "Microsoft.FSharp.Core"
@@ -132,26 +145,18 @@ type internal Testing() =
 
         let rec toString useFullName (t: Type) =
 
-            let hasUnitOfMeasure = 
-                t.IsGenericType && 
-                (t.Name = typeof<bool>.Name ||
-                 t.Name = typeof<obj>.Name ||
-                 t.Name = typeof<int>.Name ||
-                 t.Name = typeof<int64>.Name ||
-                 t.Name = typeof<float>.Name ||
-                 t.Name = typeof<float32>.Name ||
-                 t.Name = typeof<decimal>.Name)
+            let hasUnit =  hasUnitOfMeasure t
 
             let innerToString (t: Type) =
                 match t with
-                | _ when t.Name = typeof<bool>.Name && not hasUnitOfMeasure -> "bool"
-                | _ when t.Name = typeof<obj>.Name && not hasUnitOfMeasure  -> "obj"
-                | _ when t.Name = typeof<int>.Name && not hasUnitOfMeasure  -> "int"
-                | _ when t.Name = typeof<int64>.Name && not hasUnitOfMeasure  -> "int64"
-                | _ when t.Name = typeof<float>.Name && not hasUnitOfMeasure  -> "float"
-                | _ when t.Name = typeof<float32>.Name && not hasUnitOfMeasure  -> "float32"
-                | _ when t.Name = typeof<decimal>.Name && not hasUnitOfMeasure  -> "decimal"
-                | _ when t.Name = typeof<string>.Name && not hasUnitOfMeasure  -> "string"
+                | _ when t.Name = typeof<bool>.Name && not hasUnit -> "bool"
+                | _ when t.Name = typeof<obj>.Name && not hasUnit  -> "obj"
+                | _ when t.Name = typeof<int>.Name && not hasUnit  -> "int"
+                | _ when t.Name = typeof<int64>.Name && not hasUnit  -> "int64"
+                | _ when t.Name = typeof<float>.Name && not hasUnit  -> "float"
+                | _ when t.Name = typeof<float32>.Name && not hasUnit  -> "float32"
+                | _ when t.Name = typeof<decimal>.Name && not hasUnit  -> "decimal"
+                | _ when t.Name = typeof<string>.Name && not hasUnit  -> "string"
                 | _ when t.Name = typeof<Void>.Name -> "()"
                 | _ when t.Name = typeof<unit>.Name -> "()"
                 | t when t.IsArray -> (t.GetElementType() |> toString useFullName) + "[]"
@@ -162,7 +167,7 @@ type internal Testing() =
                     let args =
                         if useFullName then
                             t.GetGenericArguments()
-                            |> Seq.map (if hasUnitOfMeasure then (fun t -> t.Name) else toString useFullName)
+                            |> Seq.map (if hasUnit then (fun t -> t.Name) else toString useFullName)
                         else
                             t.GetGenericArguments()
                             |> Seq.map (fun _ -> "_")
@@ -174,7 +179,7 @@ type internal Testing() =
                         let args = String.concat "," args
                         let name, reverse =
                             match t with
-                            | t when hasUnitOfMeasure -> toString useFullName t.UnderlyingSystemType, false
+                            | t when hasUnit -> toString useFullName t.UnderlyingSystemType, false
                             // Short names for some known generic types
                             | t when not useQualifiedNames && t.GetGenericTypeDefinition().Name = typeof<int seq>.GetGenericTypeDefinition().Name -> "seq", true
                             | t when not useQualifiedNames && t.GetGenericTypeDefinition().Name = typeof<int list>.GetGenericTypeDefinition().Name -> "list", true
@@ -204,7 +209,7 @@ type internal Testing() =
 
             if ignoreOutput then
                 ""
-            elif hasUnitOfMeasure || t.IsGenericParameter || t.DeclaringType = null then
+            elif hasUnit || t.IsGenericParameter || t.DeclaringType = null then
                 innerToString t + (warnIfWrongAssembly t)
             else
                 (toString useFullName t.DeclaringType) + "+" + (innerToString t) + (warnIfWrongAssembly t)
@@ -483,6 +488,7 @@ type internal Testing() =
             let rec eraseType(typ:Type): Type =
                 match typ with
                 | t when t.Attributes &&& enum (int32 TypeProviderTypeAttributes.IsErased) <> enum 0 -> eraseType typ.BaseType
+                | t when isFSharpUnitAnnotated t -> eraseType typ.UnderlyingSystemType
                 | t when t.IsArray ->
                     let rank = t.GetArrayRank()
                     let et = eraseType (t.GetElementType())
