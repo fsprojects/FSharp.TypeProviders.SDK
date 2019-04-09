69f55afb25a21ba8ad60c38ca0a4b25e9c0f6e9a
Don Syme <dsyme@microsoft.com>
2019-04-09 16:42:43 +0100

    add weak cache for intra-provider sharing

--- tests/Program.fs
+++ tests/Program.fs
@@ -1,4 +1,4 @@
 module Program 
 let [<EntryPoint>] main _ = 
-   TPSDK.BasicErasedTests.``test reader cache actually caches``()
+   //TPSDK.BasicErasedTests.``test reader cache actually caches``()
    0
