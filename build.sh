768c77ac551f9dfd91ba0f9babd2241874c1c07c
cartermp <pcarter@fastmail.com>
2020-07-26 16:16:56 -0700

    Get rid of nonsense from build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# On Linux (or at least, Ubuntu), update the libunwind8 package so .NET Core can run, see https://github.com/dotnet/cli/issues/3390
-if [ $(uname -s) = 'Linux' ]; then
-    sudo apt-get install libunwind8
-fi
-
-which mono
-which msbuild
 
 dotnet tool restore
 dotnet paket restore
