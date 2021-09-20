c05296075a09ac82285e07db5bdd85d92bfe9d1d
Don Syme <dsyme@users.noreply.github.com>
2021-09-20 20:00:50 +0100

    fix build (#367)
    * fix build
    
    * fix build
    
    * fix: ci dotnet version
    
    * fix warnings
    
    * fix tests
    
    * update fake stuff
    
    * fix tests
    
    * fix tests
    
    * fix logo
    
    * include IVT
    
    * fix packing
    
    Co-authored-by: Sergey Tihon <sergey.tihon@gmail.com>

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -3,4 +3,6 @@
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("FSharp.TypeProviders.SDK.Tests")>]
+[<assembly: InternalsVisibleTo("StressProvider.Tests")>]
+[<assembly: InternalsVisibleTo("BasicProvider.Tests")>]
 do()
